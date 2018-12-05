8e1bac7b9545a510e243d480408994c50a240afe
Brendan <Brendan@10.100.140.237>
2018-12-05 14:47:35 -0600

    formatted

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,32 +12,19 @@ import main.TicTacToe;
  */
 class Main {
 
+	public static void main(String[] args) throws java.lang.Exception {
 
-  public static void main(String[] args) throws java.lang.Exception {
-    
-    
-    Brendan.printGrade(85);
-    CollegeClass cop2006 = new CollegeClass();
-    cop2006.printInfo();
-cop2006.setName("COP 2006");
-cop2006.setCredits(4);
-String[]names =new String[3];
-names[0]="brendan";
-names[1]="zacc";
-names[2]="tim";
-cop2006.setRoster(names);
-cop2006.printInfo();
+		Brendan.printGrade(85);
+		CollegeClass cop2006 = new CollegeClass();
+		cop2006.printInfo();
+		cop2006.setName("COP 2006");
+		cop2006.setCredits(4);
+		String[] names = new String[3];
+		names[0] = "brendan";
+		names[1] = "zacc";
+		names[2] = "tim";
+		cop2006.setRoster(names);
+		cop2006.printInfo();
 
-
-
-  }
+	}
 }
-    
-    
-    
-   
-    
-  
-
-
-
